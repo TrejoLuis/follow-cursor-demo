f3e201704d1a67f974c0df08ffa905fae907f7d0
Luis Trejo <trejorias@gmail.com>
2023-06-30 18:59:29 -0600

    feat: add div which will track the cursor, add style

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,21 @@ import './App.css'
 function App () {
   const [enableTrack, setEnableTrack] = useState(false)
 
+  const cursorDecoratorStyle = {
+    width: '20px',
+    height: '20px',
+    backgroundColor: 'red',
+    borderRadius: '50%',
+    position: 'absolute',
+    top: '-10px',
+    left: '-10px',
+    translate: '50px 50px'
+  }
+
   return (
     <>
       <h1>Cursor tracking demo</h1>
+      <div className="cursor-decorator" style={cursorDecoratorStyle}/>
       <button onClick={() => setEnableTrack(!enableTrack)}>{enableTrack ? 'Disable' : 'Enable'} cursor tracking</button>
     </>
   )
